591731d418213b12e1dcae760f7604f64d164634
Leticia <leticia.lima@mirumagency.com>
2023-01-05 23:40:33 -0300

    Exercicios resolvidos aula 0308

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -144,7 +144,7 @@ function handleClick2(e) {
 allElements2.forEach( (element) => {
   element.addEventListener('click', handleClick2);
 });
-*/
+
 
 // Se o usuário clicar na tecla (t), aumente todo o texto do site. 
 function handleKeyboard(event) {
@@ -156,3 +156,28 @@ function handleKeyboard(event) {
   }
 }
 window.addEventListener('keydown', handleKeyboard);
+
+
+// Aula 03.08
+// Duplique o menu e adicione ele em copy
+const menu = document.querySelector('.menu');
+const cloneMenu = menu.cloneNode(true);
+const copy = document.querySelector('.copy');
+copy.appendChild(cloneMenu);
+
+// Selecione o primeiro DT da DL de Faq
+const faq = document.querySelector('.faq-lista');
+const stDT = faq.children[0];
+console.log(stDT);
+
+
+// Selecione o DD referente ao primeiro DT
+const faq2 = document.querySelector('.faq-lista');
+const DD = faq2.children[0].nextElementSibling;
+console.log(DD);
+
+// Substitua o conteúdo html de .faq pelo de .animais
+const animais = document.querySelector('.animais');
+const faq3 = document.querySelector('.faq');
+faq3.innerHTML = animais.innerHTML;
+*/
